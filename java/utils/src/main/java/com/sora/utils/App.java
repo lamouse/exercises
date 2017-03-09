76611b9802ccda3cd03357743a3d3886b18ba9ef
sora <softsora@163.com>
2017-03-09 03:05:21 -0900

    a simple property util

---
+++ java/utils/src/main/java/com/sora/utils/App.java
@@ -0,0 +1,13 @@
+package com.sora.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
