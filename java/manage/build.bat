5b7d11dfa498c2a9d6eef9d826605ab2e5169374
sora <softsora@163.com>
2020-01-07 23:06:49 +0800

    delete java project

--- java/manage/build.bat
+++
@@ -1,7 +0,0 @@
-echo off
-
-call mvn clean
-call mvn install
-
-echo;
-pause
--- java/manage/src/main/java/com/wxc/manage/mapper/UserMapper.java
+++
@@ -1,6 +0,0 @@
-package com.wxc.manage.mapper;
-
-import com.wxc.manage.model.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
